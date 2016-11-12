86ce90e07cabdd1decb40ee119b476d1a7344893
waskars11 <doteabas@gmail.com>
2016-11-12 10:21:08 -0400

    archivos iniciales del proyecto

---
+++ objetoEjemplo.js
@@ -0,0 +1,6 @@
+var libro= new Object();
+libro.autor="paulo cohelo";
+libro.paginas="225 paginas";
+libro.año="2005";
+
+console	.log	(libro);
---
+++ tablaSuma.js
@@ -0,0 +1,13 @@
+var x=7;
+
+console.log("SUMA DE NUMEROS 7:");
+console.log("7+1 = "+ (++x));
+console.log("7+2 = "+ (++x));
+console.log("7+3 = "+ (++x));
+console.log("7+4 = "+ (++x));
+console.log("7+5 = "+ (++x));
+console.log("7+6 = "+ (++x));
+console.log("7+7 = "+ (++x));
+console.log("7+8 = "+ (++x));
+console.log("7+9 = "+ (++x));
+console.log("7+10 = "+ (++x));
